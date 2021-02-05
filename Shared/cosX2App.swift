fc66306106db9d7a79b0b9cfe800ae25e893de32
Matt <madas@hawk.iit.edu>
2021-02-05 10:19:21 -0600

    Initial Commit

---
+++ Shared/cosX2App.swift
@@ -0,0 +1,17 @@
+//
+//  cosX2App.swift
+//  Shared
+//
+//  Created by Matthew Adas on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct cosX2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
